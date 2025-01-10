b586e1d57adbe22c664a66387b5ce5b52f809898
Sylvain Ramshaw <sylvain.ramshaw@gmail.com>
2025-01-09 20:17:40 -0600

    map container to wasix

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build --tag wasm-workshop --file setup.Dockerfile .
-docker run --rm -it wasm-workshop
+docker run  --mount type=bind,src=/home/papa/_git2/wasix-rust-examples/wasix-grpc,dst=/root/wasix-grpc --rm -it wasm-workshop
